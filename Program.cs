e02ffbb7e56ea60794bf574a9e5ec75650a92e30
Youngseok Oh <gongsami4503@naver.com>
2018-08-29 10:07:57 +0900

    Earth is mine!

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("World is mine");
+            Console.WriteLine("Earth is mine");
         }
     }
 }
